4af06567888e2865fe80b668e11458a8439fe004
melvinUGO <ugochukwumelvin@gmail.com>
2022-08-24 10:41:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const tabs = document.querySelectorAll(".tab");
+const positionGroups = document.querySelectorAll(".position-group");
+const form = document.querySelector(".positions");
+
+form.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+
+  if (id) {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tabs.forEach((tab) => {
+      if (tab.dataset.id === id) {
+        tab.classList.add("active-tab");
+      }
+    });
+    positionGroups.forEach((positionGroup) => {
+      positionGroup.classList.remove("active");
+    });
+    const tab = document.getElementById(id);
+    tab.classList.add("active");
+  }
+});
